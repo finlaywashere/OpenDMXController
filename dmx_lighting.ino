11a5b58f5c73c49008daac7d9bd53e795df4eed4
finlaywashere <finman292004@protonmail.com>
2021-01-20 08:22:08 -0500

    Got it working

--- dmx_lighting.ino
+++ dmx_lighting.ino
@@ -1,58 +1,72 @@
-#include <ArduinoSoftRS485.h>
-#include <ArduinoSoftDMX.h>
+/*
+  DMX Fade
 
-#define RX1 2
-#define TX1 3
-#define RX2 4
-#define TX2 5
+  This sketch fades the value of DMX channel 1 between 0 and 255 in steps to create a fade effect.
+  All other slots are set to a value of 0.
 
-#define RE1 A4
-#define DE1 A5
-#define RE2 A6
-#define DE2 A7
+  Circuit:
+   - DMX light
+   - MKR board
+   - MKR 485 shield
+     - ISO GND connected to DMX light GND (pin 1)
+     - Y connected to DMX light Data + (pin 2)
+     - Z connected to DMX light Data - (pin 3)
+     - Jumper positions
+       - Z \/\/ Y set to ON
 
-#define UNIVERSE_SIZE 512
+  created 5 July 2018
+  by Sandeep Mistry
+*/
 
-DMXClass* dmx1;
-DMXClass* dmx2;
+#include <ArduinoRS485.h> // the ArduinoDMX library depends on ArduinoRS485
+#include <ArduinoDMX.h>
+
+const int universeSize = 512;
+
+int brightness = 0;
+int fadeAmount = 5;
+
+DMXClass *dmx_1;
+DMXClass *dmx_2;
 
 void setup() {
+  //pinMode(3,OUTPUT);
+  //digitalWrite(3,HIGH);
+
+  RS485Class *rs485_1 = new RS485Class(Serial1,18,A5,A5);
+  RS485Class *rs485_2 = new RS485Class(Serial2,16,A7,A7);
+  dmx_1 = new DMXClass(*rs485_1);
+  dmx_2 = new DMXClass(*rs485_2);
+  
   Serial.begin(9600);
-  while(!Serial);
-  
-  Serial.println("Initializing!");
 
-  CustomSoftwareSerial *cs1 = new CustomSoftwareSerial(RX1,TX1);
-  CustomSoftwareSerial *cs2 = new CustomSoftwareSerial(RX2,TX2);
-  
-  RS485Class* rs485_1 = new RS485Class(*cs1,TX1,DE1,RE1);
-  RS485Class* rs485_2 = new RS485Class(*cs2,TX2,DE2,RE2);
-
-  dmx1 = new DMXClass(*rs485_1);
-  dmx2 = new DMXClass(*rs485_2);
-  
-  if(!dmx1->begin(UNIVERSE_SIZE)){
-    Serial.println("Failed to initialize DMX #1!");
-    while(1);
+  // initialize the DMX library with the universe size
+  if (!dmx_1->begin(universeSize)) {
+    Serial.println("Failed to initialize DMX!");
+    while (1); // wait for ever
   }
-  if(!dmx2->begin(UNIVERSE_SIZE)){
-    Serial.println("Failed to initialize DMX #2!");
-    while(1);
+  if (!dmx_2->begin(universeSize)) {
+    Serial.println("Failed to initialize DMX!");
+    while (1); // wait for ever
   }
+  Serial.println("Initialized DMX!");
 }
 
 void loop() {
-  dmx1->beginTransmission();
-  dmx1->write(1,255);
-  dmx1->write(2,255);
-  dmx1->endTransmission();
+  // set the value of channel 2
+  dmx_1->beginTransmission();
+  dmx_1->write(1, 255);
+  dmx_1->write(2, brightness);
+  dmx_1->endTransmission();
 
-  delay(1000);
+  // change the brightness for the next round
+  brightness += fadeAmount;
 
-  dmx1->beginTransmission();
-  dmx1->write(1,0);
-  dmx1->write(2,0);
-  dmx1->endTransmission();
+  // reverse fade direction when on edge of range
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
 
-  delay(1000);
+  // delay for dimming effect
+  delay(30);
 }
