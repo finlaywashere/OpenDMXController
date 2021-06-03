a54457da0fe93d21cd2b6560c9ca05744eb86dc4
finlaywashere <finman292004@protonmail.com>
2021-06-03 18:03:24 -0400

    Added more commands

--- OpenDMXController.ino
+++ OpenDMXController.ino
@@ -17,6 +17,18 @@
 
 #include <ArduinoRS485.h> // the ArduinoDMX library depends on ArduinoRS485
 #include <ArduinoDMX.h>
+#include <HardwareSerial.h>
+#include <avr/boot.h>
+
+#define MAGIC_HIGH 0xBE
+#define MAGIC_LOW 0xEF
+#define PROTOCOL 2
+#define SOFT_REV_MAJOR 0
+#define SOFT_REV_MINOR 9
+#define HARD_REV 4
+
+
+void(* resetFunc) (void) = 0;
 
 const int universeSize = 512;
 
@@ -24,6 +36,7 @@ DMXClass *dmx_1;
 DMXClass *dmx_2;
 
 void setup() {
+  // Setup output ports
   RS485Class *rs485_1 = new RS485Class(Serial1,18,A5,A5);
   RS485Class *rs485_2 = new RS485Class(Serial2,16,A7,A7);
   dmx_1 = new DMXClass(*rs485_1);
@@ -32,27 +45,25 @@ void setup() {
   Serial.begin(115200);
   // Start auxilliary serial port to receive commands from addons
   Serial3.begin(115200);
-
-  // initialize the DMX library with the universe size
-  if (!dmx_1->begin(universeSize)) {
-    Serial.println("Failed to initialize DMX!");
-    while (1); // wait for ever
-  }
-  if (!dmx_2->begin(universeSize)) {
-    Serial.println("Failed to initialize DMX!");
-    while (1); // wait for ever
-  }
-  Serial.println("Initialized DMX!");
 }
 int count = 0;
 void loop() {
-  if(Serial.available() >= 5){
-    byte cmd = Serial.read();
+  if(Serial.available() || Serial3.available()){
+    HardwareSerial *serial;
+    if(Serial.available())
+      serial = &Serial;
+    else
+      serial = &Serial3;
+    
+    byte cmd = serial->read();
     if(cmd == 1){
+      // Write command
+      // Wait for the data
+      while(serial->available() < 4){}
       // Send
-      int universe = Serial.read();
-      int channel = Serial.read() | (((int)Serial.read()) << 8);
-      int value = Serial.read();
+      int universe = serial->read();
+      int channel = serial->read() | (((int)serial->read()) << 8);
+      int value = serial->read();
       if(universe == 1){
         dmx_1->beginTransmission();
         dmx_1->write(channel,value);
@@ -62,12 +73,40 @@ void loop() {
         dmx_2->write(channel,value);
         dmx_2->endTransmission();
       }
-      Serial.write(cmd);
-      Serial.write(universe);
-      Serial.write(((byte) channel));
-      Serial.write(value);
+      serial->write(1);
+    }else if(cmd == 2){
+      // SN command
+      serial->write(2);
+      for (uint8_t i = 14; i < 24; i += 1) {
+        serial->print(boot_signature_byte_get(i), HEX);
+      }
+    }else if(cmd == 3){
+      // Reset command
+      serial->write(3);
+      resetFunc();
+    }else if(cmd == 4){
+      // Identify command
+      serial->write(4);
+      serial->write(MAGIC_HIGH);
+      serial->write(MAGIC_LOW);
+      serial->write(PROTOCOL);
+      serial->write(SOFT_REV_MAJOR);
+      serial->write(SOFT_REV_MINOR);
+      serial->write(HARD_REV);
+    }else if(cmd == 5){
+      // Initialize command
+      // This isn't in the setup function because the serial stuff isn't necessarily connected during the setup phase and errors need to be reported (so its not just broken mysteriously)
+      serial->write(5);
+      // initialize the DMX library with the universe size
+      int code = 0;
+      if (!dmx_1->begin(universeSize)) {
+        code = 253;
+      }
+      if (!dmx_2->begin(universeSize)) {
+        code = 254;
+      }
+      serial->write(code); // This is currently the only self testing supported (maybe more will be added in future revisions
     }
-    Serial.write('B');
   }
   count++;
   if(count >= 50){
