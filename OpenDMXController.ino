d0ecfa5432f8bdcf531be731134266f3e2b109d2
finlaywashere <finman292004@protonmail.com>
2021-06-03 16:35:48 -0400

    Got firmware working and fixed weird serial issues that required hacky fixes

--- OpenDMXController.ino
+++ OpenDMXController.ino
@@ -18,63 +18,64 @@
 #include <ArduinoRS485.h> // the ArduinoDMX library depends on ArduinoRS485
 #include <ArduinoDMX.h>
 
-#define LED3 40
-#define LED2 39
-#define LED1 4
-
 const int universeSize = 512;
 
 DMXClass *dmx_1;
 DMXClass *dmx_2;
 
 void setup() {
-  pinMode(LED3,OUTPUT);
-  pinMode(LED2,OUTPUT);
-  pinMode(LED1,OUTPUT);
   RS485Class *rs485_1 = new RS485Class(Serial1,18,A5,A5);
   RS485Class *rs485_2 = new RS485Class(Serial2,16,A7,A7);
   dmx_1 = new DMXClass(*rs485_1);
   dmx_2 = new DMXClass(*rs485_2);
   
-  Serial.begin(57600);
-  
+  Serial.begin(115200);
+  // Start auxilliary serial port to receive commands from addons
+  Serial3.begin(115200);
+
   // initialize the DMX library with the universe size
   if (!dmx_1->begin(universeSize)) {
-    //Serial.println("Failed to initialize DMX!");
-    digitalWrite(LED3,HIGH);
+    Serial.println("Failed to initialize DMX!");
     while (1); // wait for ever
   }
   if (!dmx_2->begin(universeSize)) {
-    //Serial.println("Failed to initialize DMX!");
-    digitalWrite(LED3,HIGH);
+    Serial.println("Failed to initialize DMX!");
     while (1); // wait for ever
   }
   Serial.println("Initialized DMX!");
-  digitalWrite(LED2,HIGH);
 }
-
+int count = 0;
 void loop() {
-  dmx_1->beginTransmission();
-  dmx_2->beginTransmission();
-  
-  Serial.print('a'); // I do not know why this works but it stops all my serial comms from breaking
-  if(Serial.available()){
-    digitalWrite(LED1,HIGH);
+  if(Serial.available() >= 5){
     byte cmd = Serial.read();
-    if(cmd == 1 || cmd == '1'){
+    if(cmd == 1){
       // Send
-      byte universe = Serial.read();
+      int universe = Serial.read();
       int channel = Serial.read() | (((int)Serial.read()) << 8);
-      byte value = Serial.read();
+      int value = Serial.read();
       if(universe == 1){
+        dmx_1->beginTransmission();
         dmx_1->write(channel,value);
+        dmx_1->endTransmission();
       }else if(universe == 2){
+        dmx_2->beginTransmission();
         dmx_2->write(channel,value);
+        dmx_2->endTransmission();
       }
+      Serial.write(cmd);
+      Serial.write(universe);
+      Serial.write(((byte) channel));
+      Serial.write(value);
     }
-    Serial.print('a');
+    Serial.write('B');
   }
-  dmx_1->endTransmission();
-  dmx_2->endTransmission();
-  delay(100);
+  count++;
+  if(count >= 50){
+    count = 0;
+    dmx_1->beginTransmission();
+    dmx_2->beginTransmission();
+    dmx_1->endTransmission();
+    dmx_2->endTransmission();
+  }
+  delay(1);
 }
